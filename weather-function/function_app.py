2711eaf85e69c437991e87644f888d055e797eed
Piotr Karpala <piotrkarpala@microsoft.com>
2025-08-04 21:58:38 -0400

    function app

---
+++ weather-function/function_app.py
@@ -0,0 +1,37 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+import httpx
+
+app = func.FunctionApp()
+
+
+@app.queue_trigger(arg_name="msg", queue_name="azure-function-weather-input", connection="STORAGE_CONNECTION")
+@app.queue_output(arg_name="outputQueue", queue_name="azure-function-weather-output", connection="STORAGE_CONNECTION")
+
+async def check_weather(msg: func.QueueMessage, outputQueue: func.Out[str]):
+    try:
+        msg_body = msg.get_body().decode("utf-8")
+        logging.info('Python Queue trigger processed a message: %s', msg_body)
+        messagepayload = json.loads(msg_body)
+        logging.info(f'The function receives the following message: {json.dumps(messagepayload)}')
+        location = messagepayload["location"]
+
+        # Make request to wttr.in
+        wttrUrl = f"https://wttr.in/{location}?format=j1"
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(wttrUrl)
+            weather_data = response.json()
+
+        response_message = {
+            "Value": weather_data,
+            "CorrelationId": messagepayload["CorrelationId"]
+        }
+        logging.info(f'The function returns the following message through the {outputQueue} queue')
+
+        outputQueue.set(json.dumps(response_message))
+
+    except Exception as e:
+        logging.error(f"Error processing message: {e}")
